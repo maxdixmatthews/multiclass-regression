ae62105ef9b3fa4b6b0d937d9a85b025528c0652
maxdixmatthews <maxdixmatthews@gmail.com>
2024-01-11 00:20:21 +0800

    Cleanup old comments and print statements

--- regression_neat.py
+++ regression_neat.py
@@ -23,26 +23,15 @@ def main(config):
     X1_train, X1_test, y1_train, y1_test = train_test_split(df, df['Y'], test_size=0.2, random_state=42)    
     # #build tree_model
     tree_len = 4
-    # i = [[(3, 4), ], [(1, 3, 4), (2,)], [(3,), (4,)]]
-    # # i = [[(1,), (2, 3, 4)], [(2,), (3, 4)], [(3,), (4,)]]
-    # all_tree_struc = mf.defined_all_trees(4)
-    # print()
-    # all_model_struc = mf.single_models_from_trees(all_tree_struc)
-    # all_models = mf.build_single_models(all_model_struc, X1_train)
-    # print(all_models)
-    # mod_in_tree = [all_models.get(tuple(lst)) for lst in i if tuple(lst) in all_models]
-    # for i in mod_in_tree:
-    #     print(i.name)
-    # return
-
     all_tree_struc = mf.defined_all_trees(4)
     all_model_struc = mf.single_models_from_trees(all_tree_struc)
     all_models = mf.build_single_models(all_model_struc, X1_train)
     n=1
     for tree in all_tree_struc:
         mod_in_tree = [all_models.get(tuple(lst)) for lst in tree if tuple(lst) in all_models]
-        if mod_in_tree != tree_len -1:
+        if len(mod_in_tree) != tree_len -1:
             print(f"Error in {tree} as only have {mod_in_tree}")
+            continue
         tree_model = mod.tree_model('tree_mod1', mod_in_tree, tree)
         output = tree_model.predict(X1_test)
         print(f"Accuracy for tree {sorted(tree, key=lambda x:len(str(x[0])+str(x[1])), reverse=True)} is: {accuracy_score(y1_test.to_list(), output['y_pred'].to_list())}")
