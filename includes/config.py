a1bb483ce01f5fdd5bd134e4fb50cd3993c2831e
maxdixmatthews <maxdixmatthews@gmail.com>
2024-12-08 19:04:21 +0800

    Restructure for push to pypi

--- includes/config.py
+++
@@ -1,7 +0,0 @@
-from includes.logging import setup_logger
-
-class Config:
-    def __init__(self, dataset):
-        self.settings = {}
-        self.model_path = 'C:\\Users\\maxdi\\OneDrive\\Documents\\uni_honours\\models'
-        self.log = setup_logger(dataset)
